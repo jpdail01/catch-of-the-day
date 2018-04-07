651fd6965986e09c22a48a5163745eca4f7e9687
Jason Dailey <jason.dailey@skuvault.com>
2018-04-07 19:25:37 -0400

    Through Video 16

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
 import sampleFishes from '../sample-fishes'
+import Fish from './Fish'
 
 class App extends React.Component{
   state = {
@@ -26,6 +27,16 @@ class App extends React.Component{
         fishes: sampleFishes
       }, () => {}
     );
+  };
+
+  addToOrder = (key) => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState(
+      {
+        order
+      }, () => {}
+    );
   }
 
   render(){
@@ -33,6 +44,12 @@ class App extends React.Component{
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map(key => 
+              <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+              )
+            }
+          </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
