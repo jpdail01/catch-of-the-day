41cdfa05467bdc199aed509d926016728613b06f
Jason Dailey <jason.dailey@skuvault.com>
2018-04-28 15:45:25 -0400

    Through Video 23, only a few PropTypes

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
+import PropTypes from 'prop-types';
 
 class Fish extends React.Component{
+  static propTypes = {
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      price: PropTypes.number,
+      description: PropTypes.string,
+      status: PropTypes.string,
+    }),
+    addToOrder: PropTypes.func,
+  }
   render(){
     const {image, name, desc, price, status} = this.props.details;
     const isAvailable = status === 'available';
